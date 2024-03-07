56524835e90f485df7c7a37e0f491e8d6368d541
trentmbx <trentmbx@gmail.com>
2024-03-07 16:56:01 -0500

    Update 2
    - Added directional movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,13 +10,55 @@ public class PlayerController : MonoBehaviour
     public float walkSpeed = 5F;
     Vector2 moveInput;
 
-    public bool IsMoving { get; private set; }
+    private bool _isMoving = false;
+
+    // Property for getting the current move speed
+    public float CurrentMoveSpeed { get
+        {
+            if (IsMoving)
+            {
+                return walkSpeed;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    // Property for whether character is moving (also updates animator moving value)
+    public bool IsMoving { get
+        {
+            return _isMoving;
+        }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
+        } 
+    }
+
+    public bool _isFacingRight = true;
+
+    // Property checking direction character is facing
+    public bool IsFacingRight { get { return _isFacingRight; } private set{
+            if (_isFacingRight != value)
+            {
+                transform.localScale *= new Vector2(-1, 1);
+            }
+
+            _isFacingRight = value;
+        }
+    }
 
     Rigidbody2D rb;
+    Animator animator;
 
     private void Awake()
     {
+        // Assign vars
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -27,7 +69,8 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+        // Update rigidbody velocity with walk speed
+        rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
     }
 
     // Update is called once per frame
@@ -38,8 +81,25 @@ public class PlayerController : MonoBehaviour
 
     public void OnMove(InputAction.CallbackContext context)
     {
+        // Read input action context values
         moveInput = context.ReadValue<Vector2>();
-
+        // Update whether character is moving or not
         IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            // face right
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            // face left
+            IsFacingRight = false;
+        }
     }
 }
